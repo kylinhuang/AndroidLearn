e5e6f84fbd5295182d95c3ab263789b02a9ae53c
kylinhuang <kylinhuang1029@sina.cn>
2017-01-18 10:27:47 +0800

    #Camera

--- ionic/snap/src/pages/hello-ionic/hello-ionic.ts
+++ ionic/snap/src/pages/hello-ionic/hello-ionic.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+import { HomePage } from '../home/home';
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  tab1Root: any = HomePage;
+  tab2Root: any = AboutPage;
+  tab3Root: any = ContactPage;
+  constructor( ) {
 
   }
 }
